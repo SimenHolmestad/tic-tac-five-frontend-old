6f7728749d0ea34941751d9da6b916900aca4108
Simen Holmestad <simehol@stud.ntnu.no>
2020-01-06 22:50:39 +0100

    Show next player to move or game winner below the board

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { useEffect } from 'react';
 import Board from './components/Board';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { updateGameId } from './actions/GameIdActions';
 
 function App() {
   const dispatch = useDispatch();
 
-  // Set the game id to the id described in the .env file
+  // Set the game id to the id described in the .env file when the page loads
   useEffect(() => {
     dispatch(updateGameId(process.env.REACT_APP_TEST_GAME_ID));
   }, [dispatch]);
 
+  const winner = useSelector((state) => state.gameData.winner);
+  let winnerText;
+  if (winner !== null && winner !== undefined) {
+    winnerText = <h2>winner: { winner }</h2>;
+  }
+
+  const nextToMove = useSelector((state) => state.gameData.nextToMove);
+  let nextToMoveText;
+  if (nextToMove !== null && nextToMove !== undefined) {
+    winnerText = <h2>Next To Move: { nextToMove }</h2>;
+  }
+
   return (
     <div className="App">
       <Board/>
+      { winnerText }
+      { nextToMoveText }
     </div>
   );
 }
