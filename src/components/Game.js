8face7b01113e213d2892f15a568bc7881218f20
Simen Holmestad <simehol@stud.ntnu.no>
2020-01-08 19:09:04 +0100

    Move game logic to game component

---
+++ src/components/Game.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import Board from './Board';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { updateGameId } from './../actions/GameIdActions';
+
+function Game(props) {
+  const dispatch = useDispatch();
+
+  // Set the game id to the id described in the .env file when the page loads
+  useEffect(() => {
+    dispatch(updateGameId(process.env.REACT_APP_TEST_GAME_ID));
+  }, [dispatch]);
+
+  const winner = useSelector((state) => state.gameData.winner);
+  let winnerText;
+  if (winner !== null && winner !== undefined) {
+    winnerText = <h2>winner: { winner }</h2>;
+  }
+
+  const nextToMove = useSelector((state) => state.gameData.nextToMove);
+  let nextToMoveText;
+  if (nextToMove !== null && nextToMove !== undefined) {
+    winnerText = <h2>Next To Move: { nextToMove }</h2>;
+  }
+
+  return (
+    <div>
+      <Board/>
+      { winnerText }
+      { nextToMoveText }
+    </div>
+  );
+}
+
+export default Game;
